0bf528977a645151cde4b143be4bfd5a5586b931
skrushna1704 <skrushna1704@gmail.com>
2025-06-23 22:49:04 +0530

    Remove secrets from history

--- skillswap/src/components/layout/HeaderConditional.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { usePathname } from 'next/navigation';
-import { Header } from '@/components/layout';
-
-export default function HeaderConditional() {
-  const pathname = usePathname();
-  const isDashboard = /^\/(dashboard|skills|browse-skills|exchanges|messages|community|settings|profile)(\/|$)/.test(pathname);
-  return !isDashboard ? <Header /> : null;
-} 
